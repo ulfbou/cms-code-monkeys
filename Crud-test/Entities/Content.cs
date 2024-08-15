753d42aca8793a887da7d0013d880396dbcfecd8
Ezeih Alrashdan <ezeih.alrashdan@gmail.com>
2024-08-15 15:17:20 +0200

    skapat content page

--- Crud-test/Entities/Content.cs
+++ Crud-test/Entities/Content.cs
@@ -3,11 +3,11 @@
     public class Content
     {
         public int Id { get; set; }
-        required public string Title { get; set; }
-        required public string ContentType { get; set; }
-        required public string Body { get; set; }
-        required public DateTime CreatedDate { get; set; }
-        required public DateTime LastModifiedDate { get; set; }
+         public string Title { get; set; }
+         public string ContentType { get; set; }
+         public string Body { get; set; }
+         public DateTime CreatedDate { get; set; }
+         public DateTime LastModifiedDate { get; set; }
 
         public int? PageId { get; set; }
     }
